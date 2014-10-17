05502d004261cea16c48fa042dc6d2e3f3d17a61
Mariusz Mączkowski <mari6274@wp.pl>
2014-10-17 22:44:15 +0200

    - Dodana Klasa Object do reprezentacji obiektów na mapie - Dodana tekstura reprezentująca drzewo

---
+++ include/Object.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <SFML/Graphics.hpp>
+
+class Object : public sf::Sprite
+{
+    public:
+        Object(const sf::Texture &);
+    protected:
+    private:
+};
+
+#endif // OBJECT_H
