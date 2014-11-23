282ffe6c2d30fd83e4cbd6246a1b781c1dbdbf4b
Deinonzch <slagol@st.amu.edu.pl>
2014-11-23 21:50:27 +0100

    Poprawki interpretera i próba stworzenia klasy AIML

--- src/AIML.cpp
+++ src/AIML.cpp
@@ -1,16 +1,7 @@
-#include "Game.h"
-#include <SFML/System.hpp>
-#include <iostream>
-#include <cstdio>
-#include<fstream>
-#include <vector>
-#include<map>
-#include"Interpreter.h"
-#define DEBUG true
 #include "AIML.h"
-
-
-void interpreter(TaskManager *);
+AIML::AIML()
+{
+/*    void interpreter(TaskManager *);
 inline std::string trim(std::string& str)
 {
     str.erase(0, str.find_first_not_of(' '));       //prefixing spaces
@@ -213,43 +204,12 @@ string znajdowanie(string a)
 
 if(znajdz[a]=="") return ("nie rozpoznano komendy");
 else return znajdz[a];
+}*/
+
+
 }
 
-
-using namespace std;
-int main()
+AIML::~AIML()
 {
-    Interpreter i;
-    i.interpretuj();
-
-
-
-
-    Game g;
-    sf::Thread thread(&Game::start, &g);
-    thread.launch();
-
-    TaskManager * tm = g.getTaskManager();
-
-    sf::Thread thread2(&interpreter, tm);
-    thread2.launch();
-}
-
-void interpreter(TaskManager * tm) {
-    std::string command;
-    while (true)
-    {
-        std::cout << " >>> ";
-        getline(std::cin, command);
-        //cout<<znajdowanie(command);
-
-        if (command == "idz 10 w lewo") if (!tm->move(-10, 0)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "idz 10 w prawo") if (!tm->move(10, 0)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "idz 10 w gore") if (!tm->move(0, -10)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "idz 10 w dol") if (!tm->move(0, 10)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "idz do 0 0") if (!tm->goCoordinates(0, 0)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "auto 0 0") if (!tm->goCoordinates(0, 0, true)) std::cout << tm->getError().toAnsiString() << std::endl;
-        if (command == "rozlacz") tm->quit();
-        //...
-    }
+    //dtor
 }
--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,5 +1,9 @@
 #include "Interpreter.h"
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string.h>
+#include <string>
 Interpreter::Interpreter()
 {
 
@@ -10,9 +14,32 @@ Interpreter::~Interpreter()
     //dtor
 }
 
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
 void Interpreter::interpretuj()
 {
-    InterpMorf *nazwa;
-    nazwa=morfeusz_analyse("Mam tak samo jak ty");
-    std::cout<<nazwa->forma<<"  "<<nazwa->haslo;
+    std::string polecenie;
+    std::cout << "wpisz polecenie: ";
+    getline(std::cin, polecenie);
+    std::vector<std::string> v;
+    split(polecenie, ' ', v);
+
+    for (std::string x : v)
+    {
+        char * cstr = new char[x.length()+1];
+        strcpy(cstr, x.c_str());
+        InterpMorf *wyraz;
+
+        wyraz=morfeusz_analyse(cstr);
+        std::cout << wyraz->forma << " " << wyraz->haslo << std::endl;
+    }
+
+
 }
